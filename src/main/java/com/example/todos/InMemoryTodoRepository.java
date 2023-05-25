ef9f6964a84d9f773f7f2dabeeebf53b0cbe6d87
JISOO PARK <jso9603@gmail.com>
2023-05-25 19:37:21 +0900

    todo list api 추가

--- src/main/java/com/example/todos/InMemoryTodoRepository.java
+++ src/main/java/com/example/todos/InMemoryTodoRepository.java
@@ -1,9 +1,9 @@
 // com/example/menu/item/InMemoryItemRepository.java
 
-package com.example.menu.item;
+package com.example.todos;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface InMemoryItemRepository extends CrudRepository<Item, Long> {}
+public interface InMemoryTodoRepository extends CrudRepository<Todo, Long> {}
